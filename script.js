39303fa808a5692b6fa3d8f2ecc5189766041f31
SciWiFi <40919523+SciWiFi@users.noreply.github.com>
2018-08-25 20:11:42 +0200

    Changed UI

--- script.js
+++ script.js
@@ -1,30 +1,52 @@
 var a = ["Rock", "Paper", "Scissors"];
 var res = document.querySelector("#results");
+var round = 0;
+var roundDiv = document.querySelector("#roundDiv");
 var computerSelection = "";
 var playerSelection = "";
-var buttons = document.querySelectorAll("button");
+var buttons = document.querySelectorAll(".play"); //document.querySelectorAll("button");
+var points = document.querySelector("#points");
+var playerPoints = 0;
+var computerPoints = 0;
+var winner = "";
 
 buttons.forEach((button) => {
     button.addEventListener("click", (e) => {
         playerSelection = a[button.id];
-        var winner = "";
         computerSelection = computerPlay();
         if(computerSelection == playerSelection) {
-            winner = "draw";
+            winnerText = "draw";
+            winner = "";
         }
         var win = playerSelection == "Rock" && computerSelection == "Scissors" || playerSelection == "Paper" && computerSelection == "Rock" || playerSelection == "Scissors" && computerSelection == "Paper";
         if(win) {
-            winner = "You win! " + playerSelection + " beats " + computerSelection;
+            winnerText = "You win! " + playerSelection + " beats " + computerSelection;
+            winner = "p";
+            playerPoints++;
         }
         else if(!win && !(computerSelection == playerSelection)) {
-            winner = "You lose! " + computerSelection + " beats " + playerSelection;
+            winnerText = "You lose! " + computerSelection + " beats " + playerSelection;
+            winner = "c";
+            computerPoints++;
         }
-        showWinner(winner);
+        round++;
+        showWinner(winnerText);
     });
 });
 
 function showWinner(text) {
-    res.textContent = text;
+    //res.textContent = text + "\n" + res.textContent;
+    if(winner == "") {
+    	res.innerHTML = "<p>" + text + "</p>" + res.innerHTML;
+    }
+    else if(winner == "p") {
+    	res.innerHTML = "<p class='won'>" + text + "</p>" + res.innerHTML;
+    }
+    else if(winner == "c") {
+    	res.innerHTML = "<p class='lose'>" + text + "</p>" + res.innerHTML;
+    }
+    roundDiv.innerHTML = "Round: " + round;
+    points.innerHTML = "You: " + playerPoints + " Computer: " + computerPoints;
 }
 
 function computerPlay() {
